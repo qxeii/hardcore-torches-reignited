d227fc467585877c0dbd5b3857bd3681860b9da3
August Saint Freytag <me@augustfreytag.com>
2024-04-21 00:08:09 +0200

    Rewrite inventory utils, lighter find and sort logic

--- src/main/java/net/qxeii/hardcore_torches/util/InventoryUtils.java
+++ src/main/java/net/qxeii/hardcore_torches/util/InventoryUtils.java
@@ -1,13 +1,9 @@
 package net.qxeii.hardcore_torches.util;
 
-import java.util.HashMap;
 import java.util.List;
 
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.tag.TagKey;
-import net.minecraft.util.collection.DefaultedList;
 import net.qxeii.hardcore_torches.Mod;
 
 public class InventoryUtils {
@@ -25,10 +21,29 @@ public class InventoryUtils {
 
 	// Items from Inventory
 
-	public static HashMap<TagKey<Item>, List<ItemStack>> getFireStarterItemsByTag(PlayerInventory inventory) {
-		HashMap<TagKey<Item>, List<ItemStack>> items = new HashMap<TagKey<Item>, List<ItemStack>>();
-		List<TagKey<Item>> tags = List.of(Mod.UNBREAKING_LIGHTER_ITEMS, Mod.SINGLE_USE_LIGHTER_ITEMS,
-				Mod.MULTI_USE_LIGHTER_ITEMS);
+	public static ItemStack getFirstUsableFireStarterItemFromInventory(PlayerInventory inventory) {
+		var map = getFireStarterItemsMap(inventory);
+		return getFirstUsableFireStarterItemFromMap(map);
+	}
+
+	public static ItemStack getFirstUsableFireStarterItemFromMap(FireStarterItemMap map) {
+		if (!map.unbreaking.isEmpty()) {
+			return map.unbreaking.get(0);
+		}
+
+		if (!map.singleUse.isEmpty()) {
+			return map.singleUse.get(0);
+		}
+
+		if (!map.multiUse.isEmpty()) {
+			return map.multiUse.get(0);
+		}
+
+		return null;
+	}
+
+	public static FireStarterItemMap getFireStarterItemsMap(PlayerInventory inventory) {
+		var map = FireStarterItemMap.of();
 
 		List<ItemStack> allInventoryStacks = List.of();
 
@@ -36,31 +51,51 @@ public class InventoryUtils {
 		allInventoryStacks.addAll(inventory.offHand);
 
 		for (ItemStack stack : allInventoryStacks) {
-			for (TagKey<Item> tag : tags) {
-				if (!stack.isIn(tag)) {
-					continue;
-				}
+			map.addStackIfMatching(stack);
+		}
 
-				if (items.containsKey(tag)) {
-					items.get(tag).add(stack);
-				} else {
-					items.put(tag, DefaultedList.of());
-					items.get(tag).add(stack);
-				}
+		map.sortAllListsByDamageValue();
+		return map;
+	}
+
+	public static class FireStarterItemMap {
+		public List<ItemStack> unbreaking;
+		public List<ItemStack> singleUse;
+		public List<ItemStack> multiUse;
+
+		public FireStarterItemMap(List<ItemStack> unbreaking, List<ItemStack> singleUse, List<ItemStack> multiUse) {
+			this.unbreaking = unbreaking;
+			this.singleUse = singleUse;
+			this.multiUse = multiUse;
+		}
+
+		public static FireStarterItemMap of() {
+			return new FireStarterItemMap(List.of(), List.of(), List.of());
+		}
+
+		public void addStackIfMatching(ItemStack stack) {
+			if (stack.isIn(Mod.UNBREAKING_LIGHTER_ITEMS)) {
+				unbreaking.add(stack);
+			} else if (stack.isIn(Mod.SINGLE_USE_LIGHTER_ITEMS)) {
+				singleUse.add(stack);
+			} else if (stack.isIn(Mod.MULTI_USE_LIGHTER_ITEMS) && stack.getDamage() < stack.getMaxDamage() - 1) {
+				multiUse.add(stack);
 			}
 		}
 
-		// Sort items in map by damage value, highest damage first.
+		public void sortAllListsByDamageValue() {
+			unbreaking.sort((ItemStack a, ItemStack b) -> {
+				return b.getDamage() - a.getDamage();
+			});
 
-		for (TagKey<Item> tag : tags) {
-			if (items.containsKey(tag)) {
-				items.get(tag).sort((ItemStack a, ItemStack b) -> {
-					return b.getDamage() - a.getDamage();
-				});
-			}
+			singleUse.sort((ItemStack a, ItemStack b) -> {
+				return b.getDamage() - a.getDamage();
+			});
+
+			multiUse.sort((ItemStack a, ItemStack b) -> {
+				return b.getDamage() - a.getDamage();
+			});
 		}
-
-		return items;
 	}
 
 }
