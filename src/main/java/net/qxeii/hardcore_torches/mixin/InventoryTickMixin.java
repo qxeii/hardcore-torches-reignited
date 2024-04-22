7cc07b7fbf701e908cf5d38a9a4d4ccf65422a00
August Saint Freytag <me@augustfreytag.com>
2024-04-22 13:45:06 +0200

    Rewrite inventory tick mixin for all items
    Restructure all blocks, separate logic for item types.
    Allow torches and lanterns to smolder, extinguish, and burn out.
    Use new logic added to torch and lantern item classes.

--- src/main/java/net/qxeii/hardcore_torches/mixin/InventoryTickMixin.java
+++ src/main/java/net/qxeii/hardcore_torches/mixin/InventoryTickMixin.java
@@ -11,16 +11,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.qxeii.hardcore_torches.Mod;
 import net.qxeii.hardcore_torches.item.GlowstoneItem;
 import net.qxeii.hardcore_torches.item.LanternItem;
@@ -41,165 +38,90 @@ public abstract class InventoryTickMixin {
 
 	@Inject(at = @At("TAIL"), method = "tick")
 	private void tick(CallbackInfo info) {
-		if (this.getServerWorld().isClient) {
-			return;
-		}
-
+		ServerWorld world = getServerWorld();
 		ServerPlayerEntity player = ((ServerPlayerEntity) (Object) this);
 		PlayerInventory inventory = player.getInventory();
 
-		for (int i = 0; i < inventory.size(); i++) {
-			tickItem(inventory, i);
+		if (world.isClient) {
+			return;
 		}
-
-		waterCheck(player, inventory);
-	}
-
-	private void waterCheck(ServerPlayerEntity player, PlayerInventory inventory) {
-		BlockPos pos = player.getBlockPos();
-		int rainEffect = Mod.config.invExtinguishInRain;
-		boolean doRain = rainEffect > 0 && player.getWorld().hasRain(pos);
 
 		for (int i = 0; i < inventory.size(); i++) {
-			ItemStack stack = inventory.getStack(i);
-			Item item = stack.getItem();
-
-			// Torches
-			if (item instanceof TorchItem) {
-				TorchItem torchItem = (TorchItem) item;
-				boolean rain = doRain;
-				boolean mainOrOffhand = (i == inventory.selectedSlot || inventory.offHand.get(0) == stack);
-				if (rainEffect == 1 && doRain)
-					rain = mainOrOffhand ? true : false;
-
-				// Rain
-				if (rain)
-					rainTorch(i, torchItem, stack, inventory, player.getWorld(), pos);
-
-				// Underwater
-				if (Mod.config.invExtinguishInWater > 0)
-					waterTorch(i, torchItem, stack, player, mainOrOffhand, pos);
-			}
+			tickItem(world, player, inventory, i);
 		}
 	}
 
-	private void waterTorch(int i, TorchItem torchItem, ItemStack stack, ServerPlayerEntity player,
-			boolean mainOrOffhand, BlockPos pos) {
-		if (!player.isSubmergedInWater()) {
-			return;
-		}
-
-		if (!((Mod.config.invExtinguishInWater == 1 && mainOrOffhand) || Mod.config.invExtinguishInWater == 2)) {
-			return;
-		}
-
-		ETorchState torchState = torchItem.getTorchState();
-		int torchConditionLoss = torchState == ETorchState.LIT ? -Mod.config.torchesExtinguishFuelLoss : 0;
-
-		if (torchState != ETorchState.LIT && torchState != ETorchState.SMOLDERING) {
-			return;
-		}
-
-		ItemStack torchStack = stack;
-		torchStack = TorchItem.modifiedStackWithState(torchStack, ETorchState.UNLIT);
-		torchStack = TorchItem.modifiedStackWithAddedFuel(torchStack, player.getWorld(), torchConditionLoss);
-		player.getInventory().setStack(i, torchStack);
-
-		player.getWorld().playSound(null, pos.up(), SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.PLAYERS, 0.5f, 1f);
-	}
-
-	private void rainTorch(int i, TorchItem torchItem, ItemStack stack, PlayerInventory inventory, World world,
-			BlockPos pos) {
-		ETorchState torchState = torchItem.getTorchState();
-
-		if (torchState == ETorchState.UNLIT) {
-			return;
-		}
-
-		ETorchState targetTorchState = torchState == ETorchState.LIT && Mod.config.torchesSmolder
-				? ETorchState.SMOLDERING
-				: ETorchState.UNLIT;
-		int torchConditionLoss = torchState == ETorchState.LIT ? -Mod.config.torchesExtinguishFuelLoss : 0;
-
-		ItemStack torchStack = stack;
-		torchStack = TorchItem.modifiedStackWithState(torchStack, targetTorchState);
-		torchStack = TorchItem.modifiedStackWithAddedFuel(torchStack, world, torchConditionLoss);
-		inventory.setStack(i, torchStack);
-
-		world.playSound(null, pos.up(), SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.PLAYERS, 0.5f, 1f);
-	}
-
-	private void tickItem(PlayerInventory inventory, int slot) {
+	private void tickItem(ServerWorld world, PlayerEntity player, PlayerInventory inventory, int slot) {
 		if (!Mod.config.tickInInventory) {
 			return;
 		}
 
-		var world = getServerWorld();
 		var stack = inventory.getStack(slot);
 		var item = stack.getItem();
 
-		// Pre-Pass: Convert vanilla items
-
 		if (Mod.config.convertVanillaTorches && item == Items.TORCH) {
-			convertVanillaTorch(stack, inventory, slot);
+			convertVanillaTorch(inventory, stack, slot);
 			return;
 		}
 
 		if (Mod.config.convertVanillaLanterns && item == Items.LANTERN) {
-			convertVanillaLantern(stack, inventory, slot);
+			convertVanillaLantern(inventory, stack, slot);
 			return;
 		}
 
 		if (item instanceof TorchItem) {
-			tickTorchItem(world, inventory, stack, slot);
+			tickTorchItem(world, player, inventory, stack, slot);
+			tickTorchItemForEnvironmentalConditions(world, player, inventory, slot);
 			return;
 		}
 
 		if (item instanceof LanternItem) {
-			tickLanternItem(inventory, stack, slot);
+			tickLanternItem(world, player, inventory, stack, slot);
 			return;
 		}
 
 		if (item instanceof ShroomlightItem) {
-			tickShroomlightItem(world, inventory, stack, slot);
+			tickShroomlightItem(world, player, inventory, stack, slot);
 			return;
 		}
 
 		if (item instanceof GlowstoneItem) {
-			tickGlowstoneItem(world, inventory, stack, slot);
+			tickGlowstoneItem(world, player, inventory, stack, slot);
 			return;
 		}
 	}
 
-	private int getFuelUse(PlayerInventory inventory, int slot) {
-		int baseFuelUse = 1;
+	// Item Conversion
 
-		if (slot == inventory.selectedSlot || slot == PlayerInventory.OFF_HAND_SLOT) {
-			return baseFuelUse * Mod.config.itemUseMultiplierWhenHeld;
-		}
-
-		return baseFuelUse;
+	private void convertVanillaTorch(PlayerInventory inventory, ItemStack stack, int slot) {
+		// Remove currently held vanilla torch stack and give a stack of
+		// same quantity of mod torches.
+		Block torchItem = Mod.config.torchesBurnWhenConverted ? Mod.LIT_TORCH : Mod.UNLIT_TORCH;
+		inventory.setStack(slot, new ItemStack(torchItem, stack.getCount()));
 	}
 
-	private void tickLanternItem(PlayerInventory inventory, ItemStack stack, int slot) {
-		var lanternItem = (LanternItem) stack.getItem();
-		var fuelUse = getFuelUse(inventory, slot);
-
-		if (!lanternItem.isLit) {
-			return;
-		}
-
-		ItemStack modifiedStack = LanternItem.addFuel(stack, getServerWorld(), -fuelUse);
-		inventory.setStack(slot, modifiedStack);
+	private void convertVanillaLantern(PlayerInventory inventory, ItemStack stack, int slot) {
+		// Remove currently held vanilla lantern stack and give a stack of
+		// same quantity of mod lanterns.
+		Block lanternItem = Mod.config.lanternsBurnWhenConverted ? Mod.LIT_LANTERN : Mod.UNLIT_LANTERN;
+		inventory.setStack(slot, new ItemStack(lanternItem, stack.getCount()));
 	}
 
-	private void tickTorchItem(ServerWorld world, PlayerInventory inventory, ItemStack stack, int slot) {
-		var state = ((TorchItem) stack.getItem()).getTorchState();
-		var fuelUse = getFuelUse(inventory, slot);
+	// Time Tick
+
+	private void tickTorchItem(ServerWorld world, PlayerEntity player, PlayerInventory inventory, ItemStack stack,
+			int slot) {
+		var item = (TorchItem) stack.getItem();
+		var state = item.getTorchState();
+		var fuelUse = getFuelUseForStack(inventory, stack);
 
 		if (state == ETorchState.LIT) {
 			ItemStack modifiedStack = TorchItem.modifiedStackWithAddedFuel(stack, world, -fuelUse);
 			inventory.setStack(slot, modifiedStack);
+
+			if (TorchItem.getFuel(modifiedStack) <= 0) {
+				item.burnOut(world, player, slot);
+			}
 
 			return;
 		}
@@ -209,15 +131,32 @@ public abstract class InventoryTickMixin {
 				ItemStack modifiedStack = TorchItem.modifiedStackWithAddedFuel(stack, world, -fuelUse);
 				inventory.setStack(slot, modifiedStack);
 			} else if (random.nextInt(Mod.config.torchesSmolderExtinguishTickChance) == 0) {
-				ItemStack modifiedStack = TorchItem.modifiedStackWithState(stack, ETorchState.UNLIT);
-				inventory.setStack(slot, modifiedStack);
+				item.extinguish(world, player, slot);
 			}
 
 			return;
 		}
 	}
 
-	private void tickShroomlightItem(ServerWorld world, PlayerInventory inventory, ItemStack stack, int slot) {
+	private void tickLanternItem(ServerWorld world, PlayerEntity player, PlayerInventory inventory, ItemStack stack,
+			int slot) {
+		var lanternItem = (LanternItem) stack.getItem();
+		var fuelUse = getFuelUseForStack(inventory, stack);
+
+		if (!lanternItem.isLit) {
+			return;
+		}
+
+		ItemStack modifiedStack = LanternItem.addFuel(stack, world, -fuelUse);
+		inventory.setStack(slot, modifiedStack);
+
+		if (LanternItem.getFuel(modifiedStack) <= 0) {
+			lanternItem.extinguish(world, player, slot);
+		}
+	}
+
+	private void tickShroomlightItem(ServerWorld world, PlayerEntity player, PlayerInventory inventory, ItemStack stack,
+			int slot) {
 		if (WorldUtils.worldIsNether(world)) {
 			ItemStack modifiedStack = ShroomlightItem.modifiedStackWithAddedFuel(stack, world, 15);
 			inventory.setStack(slot, modifiedStack);
@@ -226,7 +165,7 @@ public abstract class InventoryTickMixin {
 		}
 
 		if (WorldUtils.worldIsDaytime(world)) {
-			var fuelUse = getFuelUse(inventory, slot);
+			var fuelUse = getFuelUseForStack(inventory, stack);
 
 			ItemStack modifiedStack = ShroomlightItem.modifiedStackWithAddedFuel(stack, world, -fuelUse);
 			inventory.setStack(slot, modifiedStack);
@@ -235,7 +174,8 @@ public abstract class InventoryTickMixin {
 		}
 	}
 
-	private void tickGlowstoneItem(ServerWorld world, PlayerInventory inventory, ItemStack stack, int slot) {
+	private void tickGlowstoneItem(ServerWorld world, PlayerEntity player, PlayerInventory inventory, ItemStack stack,
+			int slot) {
 		if (WorldUtils.worldIsNether(world)) {
 			ItemStack modifiedStack = GlowstoneItem.modifiedStackWithAddedFuel(stack, world, 15);
 			inventory.setStack(slot, modifiedStack);
@@ -243,25 +183,87 @@ public abstract class InventoryTickMixin {
 			return;
 		}
 
-		var fuelUse = getFuelUse(inventory, slot);
+		var fuelUse = getFuelUseForStack(inventory, stack);
 
 		ItemStack modifiedStack = GlowstoneItem.modifiedStackWithAddedFuel(stack, world, -fuelUse);
 		inventory.setStack(slot, modifiedStack);
 	}
 
-	// Item Conversion
+	// Environmental Conditions Tick
 
-	private void convertVanillaTorch(ItemStack stack, PlayerInventory inventory, int index) {
-		// Remove currently held vanilla torch stack and give a stack of same quantity
-		// of mod torches.
-		Block torchItem = Mod.config.torchesBurnWhenConverted ? Mod.LIT_TORCH : Mod.UNLIT_TORCH;
-		inventory.setStack(index, new ItemStack(torchItem, stack.getCount()));
+	private void tickTorchItemForEnvironmentalConditions(ServerWorld world, PlayerEntity player,
+			PlayerInventory inventory, int slot) {
+		var position = player.getBlockPos();
+		var stack = inventory.getStack(slot);
+		var item = (TorchItem) stack.getItem();
+
+		if (item.getTorchState() == ETorchState.UNLIT) {
+			return;
+		}
+
+		var isRaining = worldIsRaining(world, position);
+		var isUnderwater = player.isSubmergedInWater();
+
+		var rainExtinguishStrategy = Mod.config.invExtinguishInRain;
+		var underwaterExtinguishStrategy = Mod.config.invExtinguishInWater;
+
+		// Extinguishing Strategy:
+		// `invExtinguishInWater=0`, never extinguish in water
+		// `invExtinguishInWater=1`, extinguish if held in hand
+		// `invExtinguishInWater=2`, extinguish any
+
+		boolean isInHand = stackIsInHand(inventory, stack);
+		boolean shouldExtinguishTorchInRain = isRaining && rainExtinguishStrategy != 0
+				&& (rainExtinguishStrategy == 2 || isInHand);
+		boolean shouldExtinguishTorchInWater = isUnderwater && underwaterExtinguishStrategy != 0
+				&& (underwaterExtinguishStrategy == 2 || isInHand);
+
+		// Rain
+		if (shouldExtinguishTorchInRain) {
+			extinguishTorchItemInRain(world, player, stack, slot);
+		}
+
+		// Underwater
+		if (shouldExtinguishTorchInWater) {
+			extinguishTorchItemInWater(world, player, stack, slot, isInHand);
+		}
 	}
 
-	private void convertVanillaLantern(ItemStack stack, PlayerInventory inventory, int index) {
-		// Remove currently held vanilla lantern stack and give a stack of same quantity
-		// of mod lanterns.
-		Block lanternItem = Mod.config.lanternsBurnWhenConverted ? Mod.LIT_LANTERN : Mod.UNLIT_LANTERN;
-		inventory.setStack(index, new ItemStack(lanternItem, stack.getCount()));
+	private void extinguishTorchItemInWater(ServerWorld world, PlayerEntity player, ItemStack stack, int slot,
+			boolean isInHand) {
+		var torchItem = (TorchItem) stack.getItem();
+		torchItem.extinguish(world, player, slot);
 	}
+
+	private void extinguishTorchItemInRain(ServerWorld world, PlayerEntity player, ItemStack stack, int slot) {
+		var torchItem = (TorchItem) stack.getItem();
+		var torchState = torchItem.getTorchState();
+
+		if (torchState == ETorchState.UNLIT) {
+			return;
+		}
+
+		torchItem.extinguish(world, player, slot);
+	}
+
+	// Utility
+
+	private static boolean worldIsRaining(ServerWorld world, BlockPos position) {
+		return world.hasRain(position);
+	}
+
+	private static boolean stackIsInHand(PlayerInventory inventory, ItemStack stack) {
+		return stack == inventory.getMainHandStack() || stack == inventory.offHand.get(0);
+	}
+
+	private static int getFuelUseForStack(PlayerInventory inventory, ItemStack stack) {
+		int baseFuelUse = 1;
+
+		if (stackIsInHand(inventory, stack)) {
+			return baseFuelUse * Mod.config.itemUseMultiplierWhenHeld;
+		}
+
+		return baseFuelUse;
+	}
+
 }
